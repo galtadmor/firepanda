f02a5648b564225431cf8bb8a2012e8a332bab06
Gal Tadmor <gal.tadmor@bigpanda.io>
2021-02-24 13:55:15 +0200

    Add authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth, GoogleAuthProvider } from './firebase';
+import { List } from './List';
 import './App.css';
 
-function App() {
+const App: React.FC<{}> = () => {
+  const [user, loading, error] = useAuthState(auth);
+  if (loading) {
+    return <h1>Loading...</h1>
+  }
+  if (error) {
+    return <h1>Unexpected error</h1>
+  }
+  if (!user) {
+    return (
+     <div>
+       <h1>You are not logged in!</h1>
+       <button onClick={() => {
+         auth.signInWithPopup(GoogleAuthProvider).catch();
+       }}>Log in</button>
+      </div>
+    )
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>FirePanda</h1>
+      <h3>Hello, {user.displayName}</h3>
+      <List />
+      <button onClick={() => auth.signOut()}>Log out</button>
     </div>
   );
 }
---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+  apiKey: "AIzaSyC8H9FPcZ9jwMs-xxiCbnD4ZFJiZNhvSME",
+  authDomain: "firepanda-12f07.firebaseapp.com",
+  projectId: "firepanda-12f07",
+  storageBucket: "firepanda-12f07.appspot.com",
+  messagingSenderId: "1080892993037",
+  appId: "1:1080892993037:web:0da3f155cd8e29d5e8af44",
+  measurementId: "G-KDGDRP1398"
+};
+
+export const app: firebase.app.App = firebase.initializeApp(config);
+export const auth: firebase.auth.Auth = firebase.auth();
+export const GoogleAuthProvider: firebase.auth.AuthProvider = new firebase.auth.GoogleAuthProvider();
